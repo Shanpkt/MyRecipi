b19585a8dead71bbba4330ad5171111fcbb20a85
Shantanu <shantanupathak01@gmail.com>
2023-11-24 18:07:44 +0530

    navbar

--- src/Components/searchBar/searchBar.jsx
+++ src/Components/searchBar/searchBar.jsx
@@ -4,9 +4,9 @@ import "./searchBar.scss"
 function SearchBar() {
   return (
     <div className='searchBox'>
-        <img src='' />
+        <img src='https://media.istockphoto.com/id/1313141205/photo/healthy-food-ingredients-top-view-double-side-border-on-a-wood-banner-background-copy-space.jpg?s=170667a&w=0&k=20&c=_ttuyLFDf5BoLSF47OczdigfyjBE41CPm1yaukJMQ3k=' />
 
-        <div>
+        <div className='searchBox__934'>
             <h2>hello</h2>
             <div>
                 <img/>
