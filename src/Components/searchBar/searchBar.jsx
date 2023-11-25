1a9ace6436521bebe60d1210553eff5f628d237f
Shantanu <shantanupathak01@gmail.com>
2023-11-25 11:55:25 +0530

    navbar

--- src/Components/searchBar/searchBar.jsx
+++ src/Components/searchBar/searchBar.jsx
@@ -7,9 +7,11 @@ function SearchBar() {
         <img src='https://media.istockphoto.com/id/1313141205/photo/healthy-food-ingredients-top-view-double-side-border-on-a-wood-banner-background-copy-space.jpg?s=170667a&w=0&k=20&c=_ttuyLFDf5BoLSF47OczdigfyjBE41CPm1yaukJMQ3k=' />
 
         <div className='searchBox__934'>
-            <h2>hello</h2>
-            <div>
-                <img/>
+            <h2>Find a Recipi</h2>
+            <div className='searchBox__box'>
+                <div>
+                <img className='searchBox__123'  src='data:image/svg+xml;base64,PHN2ZyB2aWV3Qm94PSIwIDAgNDIgNDIiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHBhdGggZD0iTTQwLjgxIDM1LjMybC05Ljk4Ni05Ljk4NWExNi4yNiAxNi4yNiAwIDAgMCAyLjM3My04LjQ4NUMzMy4xOTcgNy44MjIgMjUuMzc0IDAgMTYuMzQ1IDAgNy4zMiAwIDAgNy4zMiAwIDE2LjM0N2MwIDkuMDI4IDcuODIyIDE2Ljg1IDE2Ljg1IDE2Ljg1IDMgMCA1LjgxMi0uODE0IDguMjMtMi4yMjZsMTAuMDM3IDEwLjA0YTIuNTE0IDIuNTE0IDAgMCAwIDMuNTU3IDBsMi40OS0yLjQ5Yy45ODMtLjk4Mi42MjctMi4yMi0uMzU1LTMuMnpNNS4wMyAxNi4zNDZjMC02LjI1IDUuMDY3LTExLjMxNyAxMS4zMTUtMTEuMzE3IDYuMjUgMCAxMS44MiA1LjU3IDExLjgyIDExLjgyUzIzLjEgMjguMTY3IDE2Ljg1IDI4LjE2N2MtNi4yNSAwLTExLjgyLTUuNTctMTEuODItMTEuODJ6IiBmaWxsPSIjYTFhMWExIi8+PC9zdmc+'/>
+                </div>
                 <input/>
             </div>
         </div>
