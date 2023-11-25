fd8f14f6474e5531a27a44d3bc7a1c2c0a350524
Shantanu <shantanupathak01@gmail.com>
2023-11-25 22:14:58 +0530

    added ui

--- src/Components/guideBanner/guideBanner.jsx
+++ src/Components/guideBanner/guideBanner.jsx
@@ -2,13 +2,20 @@ import React from 'react'
 import "./guideBanner.scss"
 function GuideBanner() {
   return (
+    <div className='guideBox'>
     <div className='guideBanner'>
      <div className='item1'>
         <div className='item1__HEADING'>THE BIG GUIDE</div>
         <img src='https://assets.epicurious.com/photos/59bc15cfc7086b08fc83341d/9:4/w_1432,h_636,c_limit/PIE-HERO-EPI-CLASSIC-THANKSGIVING-MENU-07092017.jpg'/>
      </div>
-     <div className='item2'></div>
+     <div className='item2'>
+        <h2>Our Best ThanksGiving Recipi Tips</h2>
+        <p>All the recipes and troubleshooting advice you need for a low-stress holiday.
+                     </p>
+     </div>
      <div className='item3'></div>
+    </div>
+
     </div>
   )
 }
