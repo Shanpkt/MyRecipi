37c8efda98395988df10dbadacff6cb05861d946
Shantanu <shantanupathak01@gmail.com>
2023-11-28 23:18:11 +0530

    added ui

--- src/Components/Navbar/navbar.jsx
+++ src/Components/Navbar/navbar.jsx
@@ -6,7 +6,7 @@ function Navbar() {
   return (
     <div className='Navbar'>
         <div><a className='Navbar__108' >Epicurious</a></div>
-        <div>
+        <div className='Navbar__333'>
         <SocialIcons/>
         </div>
       
